a35fa9a8725662a26fa222f9087ce5579cb7804f
Administrator <j044290m@student.staffs.ac.uk>
2025-05-21 10:25:21 +0100

    Updated group select

--- app/src/main/java/com/example/habitapp/presentation/components/GroupSelect.kt
+++ app/src/main/java/com/example/habitapp/presentation/components/GroupSelect.kt
@@ -1,36 +1,25 @@
 package com.example.habitapp.presentation.components
 
 
-import androidx.compose.foundation.ScrollState
 import androidx.compose.foundation.border
-import androidx.compose.foundation.gestures.scrollable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.wrapContentSize
-import androidx.compose.foundation.rememberScrollState
-//import androidx.compose.material.IconButton
-//import androidx.compose.material.MaterialTheme
-import androidx.compose.material3.TextButton
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.MoreVert
-import androidx.compose.runtime.Composable
-import androidx.navigation.NavController
 import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-import java.time.LocalDate
-import kotlin.math.exp
 
 
 @Composable
